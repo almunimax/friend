38125e1952dbcecc60f4355f6cec30327622fbb5
almunimax <almunimax@mail.ru>
2022-11-30 19:29:24 +0300

    задание 5 + 4

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,8 @@ public class Main {
         System.out.println(frog);
         frog = frog / 3.5;
         System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
 
     }
 }
