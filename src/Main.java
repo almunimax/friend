2e92ac79873e97913f4d6849de88d3e9441ebb6f
almunimax <almunimax@mail.ru>
2022-11-30 19:08:37 +0300

    задание 4

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // задание 4
+        System.out.println("задание 4");
+        var friend = 19;
+        System.out.println(friend);
+    }
+}
